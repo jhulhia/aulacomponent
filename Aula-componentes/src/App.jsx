347ea49715dd0b555a79faf9bb9433b6ca0dc304
jhulhia <juliagloriasenac@gmail.com>
2025-10-22 21:50:44 -0300

    Add files via upload

---
+++ Aula-componentes/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import FirstComponent from './Componentes/FirstComponet'
+import SecondComponent from './Componentes/FirstComponet 2' 
+
+function App() {
+
+  return (
+    <div className="App">
+    <FirstComponent />
+    <SecondComponent />
+    </div>
+    
+    );
+};
+
+export default App
